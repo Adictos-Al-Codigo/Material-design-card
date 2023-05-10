9015745bc7b0dcb20a1d68722b1ba76aef010d22
Celorio2001 <jahir.celorio2001@gmail.com>
2023-05-10 00:41:35 -0500

    rating bar añadidos

--- app/src/main/java/com/jahircelorio/material_framework2/MainActivity.java
+++ app/src/main/java/com/jahircelorio/material_framework2/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnElectronica,btnPunk,btnFestivalbar, btnHiphop, btnRock;
+
+    Float valor1 = 3.50f, vaLor2 = 4.50f,valor3 = 3.0f,valor4= 5.0f;
+
+    RatingBar ratingBar1,ratingBar2,ratingBar3,ratingBar4,ratingBar5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
         btnFestivalbar = findViewById(R.id.btnFestivarbar);
         btnHiphop = findViewById(R.id.btnHiphop);
         btnRock = findViewById(R.id.btnRock);
+        ratingBar1 = findViewById(R.id.ratingBar1);
+        ratingBar1.setRating(valor1);
+
+
+        ratingBar2 = findViewById(R.id.ratingBar2);
+        ratingBar2.setRating(vaLor2);
+
+        ratingBar3 = findViewById(R.id.ratingBar3);
+        ratingBar3.setRating(valor4);
+
+        ratingBar4 = findViewById(R.id.ratingBar4);
+        ratingBar4.setRating(valor1);
+
+        ratingBar5 = findViewById(R.id.ratingBar5);
+        ratingBar5.setRating(valor1);
 
         btnElectronica.setOnClickListener(new View.OnClickListener() {
             @Override
